b3b7dc807127d5254c45723a769fc140070cfaf7
unknown <thalitaandrezza@hotmail.com>
2019-04-05 17:50:55 -0300

    FEAT: instalation of yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udaciMealsApp
+ * @description
+ * # udaciMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udaciMealsApp', [
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
